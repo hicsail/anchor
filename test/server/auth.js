39b83250d99bb4ba20ad8a8c397ac90de5a71069
Greg Frasco <frascog@bu.edu>
2017-09-20 15:50:06 -0400

    Tests for jet auth

--- test/server/auth.js
+++ test/server/auth.js
@@ -13,6 +13,7 @@ const Manifest = require('../../manifest');
 const Path = require('path');
 const Proxyquire = require('proxyquire');
 const Session = require('../../server/models/session');
+const Token = require('../../server/models/token');
 const User = require('../../server/models/user');
 
 
@@ -25,11 +26,13 @@ lab.beforeEach((done) => {
 
   stub = {
     Session: MakeMockModel(),
+    Token: MakeMockModel(),
     User: MakeMockModel()
   };
 
   const proxy = {};
   proxy[Path.join(process.cwd(), './server/models/session')] = stub.Session;
+  proxy[Path.join(process.cwd(), './server/models/token')] = stub.Token;
   proxy[Path.join(process.cwd(), './server/models/user')] = stub.User;
 
   const ModelsPlugin = {
@@ -592,6 +595,293 @@ lab.experiment('Auth Plugin Cookie', () => {
       url: '/',
       headers: {
         cookie: CookieAdmin
+      }
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.result).to.match(/ok/i);
+
+      done();
+    });
+  });
+});
+
+
+lab.experiment('Auth Plugin Token', () => {
+
+  lab.test('it returns authentication credentials', (done) => {
+
+    stub.Token.findOne = function (id, callback) {
+
+      callback(null, new Token({ _id: '2D', userId: '1D', active: true }));
+    };
+
+    stub.Token.findByIdAndUpdate = function (id, options, callback) {
+
+      callback(null, new Token({ _id: '2D', userId: '1D', active: true }));
+    };
+
+    stub.User.findById = function (username, callback) {
+
+      callback(null, new User({ _id: '1D', username: 'ren' }));
+    };
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: function (request, reply) {
+
+        server.auth.test('jwt', request, (err, credentials) => {
+
+          Code.expect(err).to.not.exist();
+          Code.expect(credentials).to.be.an.object();
+          reply('ok');
+        });
+      }
+    });
+
+    const request = {
+      method: 'GET',
+      url: '/',
+      headers: {
+        authorization: 'eyJhbGciOiJIUzI1NiJ9.NTljMmI3MTM0YjNiMDAxYjljNzk3ZTEx.r3WbN1Aat3i2JUhQaEkZQggnyBpZBCax1nBceQXGHVk'
+      }
+    };
+
+    server.inject(request, (response) => {
+
+      done();
+    });
+  });
+
+
+  lab.test('it returns an error when the token is not found', (done) => {
+
+    stub.Token.findOne = function (id, callback) {
+
+      callback();
+    };
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: function (request, reply) {
+
+        server.auth.test('jwt', request, (err, credentials) => {
+
+          Code.expect(err).to.be.an.object();
+          reply('ok');
+        });
+      }
+    });
+
+    const request = {
+      method: 'GET',
+      url: '/',
+      headers: {
+        authorization: 'eyJhbGciOiJIUzI1NiJ9.NTljMmI3MTM0YjNiMDAxYjljNzk3ZTEx.r3WbN1Aat3i2JUhQaEkZQggnyBpZBCax1nBceQXGHVk'
+      }
+    };
+
+    server.inject(request, (response) => {
+
+      done();
+    });
+  });
+
+
+  lab.test('it returns an error when the user is not found', (done) => {
+
+    stub.Token.findOne = function (id, callback) {
+
+      callback(null, new Token({ _id: '2D', userId: '1D', active: true }));
+    };
+
+    stub.Token.findByIdAndUpdate = function (id, options, callback) {
+
+      callback(null, new Token({ _id: '2D', userId: '1D', active: true }));
+    };
+
+    stub.User.findByUsername = function (username, callback) {
+
+      callback();
+    };
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: function (request, reply) {
+
+        server.auth.test('jwt', request, (err, credentials) => {
+
+          Code.expect(err).to.be.an.object();
+          reply('ok');
+        });
+      }
+    });
+
+    const request = {
+      method: 'GET',
+      url: '/',
+      headers: {
+        authorization: 'eyJhbGciOiJIUzI1NiJ9.NTljMmI3MTM0YjNiMDAxYjljNzk3ZTEx.r3WbN1Aat3i2JUhQaEkZQggnyBpZBCax1nBceQXGHVk'
+      }
+    };
+
+    server.inject(request, (response) => {
+
+      done();
+    });
+  });
+
+
+  lab.test('it returns an error when a model error occurs', (done) => {
+
+    stub.Token.findOne = function (id, callback) {
+
+      callback(Error('token fail'));
+    };
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: function (request, reply) {
+
+        server.auth.test('jwt', request, (err, credentials) => {
+
+          Code.expect(err).to.be.an.object();
+          Code.expect(credentials).to.not.exist();
+          reply('ok');
+        });
+      }
+    });
+
+    const request = {
+      method: 'GET',
+      url: '/',
+      headers: {
+        authorization: 'eyJhbGciOiJIUzI1NiJ9.NTljMmI3MTM0YjNiMDAxYjljNzk3ZTEx.r3WbN1Aat3i2JUhQaEkZQggnyBpZBCax1nBceQXGHVk'
+      }
+    };
+
+    server.inject(request, (response) => {
+
+      done();
+    });
+  });
+
+
+  lab.test('it takes over when the required role is missing', (done) => {
+
+    stub.Token.findOne = function (id, callback) {
+
+      callback(null, new Token({ _id: '2D', userId: '1D', active: true }));
+    };
+
+    stub.Token.findByIdAndUpdate = function (id, options, callback) {
+
+      callback(null, new Token({ _id: '2D', userId: '1D', active: true }));
+    };
+
+    stub.User.findById = function (id, callback) {
+
+      callback(null, new User({ _id: '1D', username: 'ren' }));
+    };
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      config: {
+        auth: {
+          strategies: ['jwt'],
+          scope: 'admin'
+        }
+      },
+      handler: function (request, reply) {
+
+        Code.expect(request.auth.credentials).to.be.an.object();
+
+        reply('ok');
+      }
+    });
+
+    const request = {
+      method: 'GET',
+      url: '/',
+      headers: {
+        authorization: 'eyJhbGciOiJIUzI1NiJ9.NTljMmI3MTM0YjNiMDAxYjljNzk3ZTEx.r3WbN1Aat3i2JUhQaEkZQggnyBpZBCax1nBceQXGHVk'
+      }
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.result.message).to.match(/insufficient scope/i);
+
+      done();
+    });
+  });
+
+
+  lab.test('it continues through pre handler when role is present', (done) => {
+
+    stub.Token.findOne = function (id, callback) {
+
+      callback(null, new Token({ _id: '2D', userId: '1D', active: true }));
+    };
+
+    stub.Token.findByIdAndUpdate = function (id, options, callback) {
+
+      callback(null, new Token({ _id: '2D', userId: '1D', active: true }));
+    };
+
+    stub.User.findById = function (id, callback) {
+
+      const user = new User({
+        username: 'ren',
+        roles: {
+          admin: {
+            id: '953P150D35',
+            name: 'Ren Höek'
+          }
+        }
+      });
+
+      user._roles = {
+        admin: {
+          _id: '953P150D35',
+          name: {
+            first: 'Ren',
+            last: 'Höek'
+          }
+        }
+      };
+
+      callback(null, user);
+    };
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      config: {
+        auth: {
+          strategies: ['jwt'],
+          scope: ['account', 'admin']
+        }
+      },
+      handler: function (request, reply) {
+
+        Code.expect(request.auth.credentials).to.be.an.object();
+
+        reply('ok');
+      }
+    });
+
+    const request = {
+      method: 'GET',
+      url: '/',
+      headers: {
+        authorization: 'eyJhbGciOiJIUzI1NiJ9.NTljMmI3MTM0YjNiMDAxYjljNzk3ZTEx.r3WbN1Aat3i2JUhQaEkZQggnyBpZBCax1nBceQXGHVk'
       }
     };
 
