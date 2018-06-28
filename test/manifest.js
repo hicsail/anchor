6d7b9d44f514061bb3c59938fe8fe55d160abf8f
Greg Frasco <frascog@bu.edu>
2018-06-28 10:31:59 -0400

    Add Test for config and manifest, updated test function

---
+++ test/manifest.js
@@ -0,0 +1,22 @@
+'use strict';
+const Code = require('code');
+const Lab = require('lab');
+const Manifest = require('../manifest');
+
+
+const lab = exports.lab = Lab.script();
+
+
+lab.experiment('Manifest', () => {
+
+  lab.test('it gets manifest data', () => {
+
+    Code.expect(Manifest.get('/')).to.be.an.object();
+  });
+
+
+  lab.test('it gets manifest meta data', () => {
+
+    Code.expect(Manifest.meta('/')).to.match(/this file defines the plot device/i);
+  });
+});
