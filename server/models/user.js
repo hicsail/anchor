2d45d6836b619c79502d5409ac17e5fa2626c33a
FrancisZamora <franciszamor@gmail.com>
2018-07-03 10:38:33 -0400

    fixing linting

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,6 @@
 'use strict';
 const Async = require('async');
 const Bcrypt = require('bcrypt');
-const Clinician = require('./clinician');
 const Joi = require('joi');
 const MongoModels = require('hicsail-mongo-models');
 
