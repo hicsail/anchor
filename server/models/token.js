581841ade74213199f4c6d3fd2f5e708721c755e
FrancisZamora <franciszamor@gmail.com>
2018-07-23 15:54:55 -0400

    token fixes

--- server/models/token.js
+++ server/models/token.js
@@ -11,24 +11,23 @@ class Token extends AnchorModels {
 
   static async generateKeyHash() {
 
-    const key = UUID();
+    const key = JWT.sign(UUID());
     const salt = await Bcrypt.gensalt(10);
     const hash = await Bcrypt.hash(key,salt);
 
     const signedKeyHash = JWT.sign({ key:hash });
-    const token = JWT.sign({ key });
 
-    return (signedKeyHash,token);
+    return (signedKeyHash);
   }
 
   static async create(document) {
 
-    const signedToken = await this.generateKeyHash()[0];
+    const keyHash = await this.generateKeyHash();
     document = {
       description: document.description,
       active: true,
       createdAt: new Date(),
-      signedToken
+      token:keyHash
 
 
 
@@ -37,6 +36,7 @@ class Token extends AnchorModels {
     };
 
     const token = await this.insertOne(document);
+    token[0].key = keyHash.key;
 
 
     return token[0];
@@ -73,6 +73,11 @@ Token.payload = Joi.object({
   active: Joi.boolean().required(),
   permission: Joi.object()
 });
+
+
+Token.indexes = [
+  { key: { userId: 1 } }
+];
 
 
 
