5cc195899e5be828b618e46520bbfa24b23da65d
FrancisZamora <franciszamor@gmail.com>
2018-07-24 12:22:35 -0400

    tests for token

---
+++ server/models/token.js
@@ -0,0 +1,87 @@
+'use strict';
+const Joi = require('joi');
+const AnchorModels = require('../anchor/anchor-model');
+const JWT = require('jsonwebtoken');
+const UUID = require('uuid/v4');
+const Bcrypt = require('bcrypt');
+
+
+
+class Token extends AnchorModels {
+
+  static async generateKeyHash() {
+
+    const key = JWT.sign({ key: UUID() }, 'secret');
+    const salt = await Bcrypt.genSalt(10);
+    const hash = await Bcrypt.hash(key,salt);
+
+    const signedKeyHash = JWT.sign({ key:hash },'secret');
+
+    return (signedKeyHash);
+  }
+
+  static async create(document) {
+
+    const keyHash = await this.generateKeyHash();
+    document = {
+      description: document.description,
+      active: true,
+      createdAt: new Date(),
+      token:keyHash,
+      userId: document.userId
+
+
+
+
+
+    };
+
+    const token = await this.insertOne(document);
+    token[0].key = keyHash.key;
+
+
+    return token[0];
+
+  }
+
+
+
+
+
+
+}
+
+Token.collectionName = 'tokens';
+
+Token.schema = Joi.object({
+  _id: Joi.object(),
+  token: Joi.string().required(),
+  userId: Joi.string().required(),
+  description: Joi.string().required(),
+  active: Joi.boolean().default(true),
+  createdAt: Joi.date(),
+  updatedAt: Joi.date(),
+  lastUsed: Joi.date(),
+  permission: Joi.object()
+
+
+
+});
+
+Token.payload = Joi.object({
+  userId: Joi.string().required(),
+  description: Joi.string().required(),
+  active: Joi.boolean().required(),
+  permission: Joi.object()
+});
+
+
+Token.indexes = [
+  { key: { userId: 1 } }
+];
+
+
+
+
+
+module.exports = Token;
---
+++ test/server/models/token.js
@@ -0,0 +1,44 @@
+'use strict';
+const Token = require('../../../server/models/token');
+const Code = require('Code');
+const Config = require('../../../config');
+const Fixtures = require('../fixtures');
+const Lab = require('lab');
+
+const lab = exports.lab = Lab.script();
+const config = Config.get('/hapiAnchorModel/mongodb');
+
+lab.experiment('Token Model', () => {
+
+  lab.before(async () => {
+
+    await Token.connect(config.connection,config.options);
+    await Fixtures.Db.removeAllData();
+  });
+
+  lab.after(async () => {
+
+    await Fixtures.Db.removeAllData();
+
+    Token.disconnect();
+  });
+
+
+  lab.test('it returns a new instance when create succeeds', async () => {
+
+    const document = {
+      userId: '1234',
+      description: 'sample token'
+
+    };
+
+    const token = await Token.create(document);
+
+    Code.expect(token).to.be.an.instanceOf(Token);
+    Code.expect(token.userId).to.equal('1234');
+    Code.expect(token.description).to.equal('sample token');
+
+  });
+
+});
+
