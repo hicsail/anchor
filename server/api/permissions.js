3054a8f5b52e15099544328dfbd7d862342afa62
FrancisZamora <franciszamor@gmail.com>
2018-08-07 16:02:41 -0400

    cleaning up

--- server/api/permissions.js
+++ server/api/permissions.js
@@ -79,10 +79,6 @@ const register = function (server, serverOptions) {
       const user = request.pre.user;
       const role = request.pre.role;
 
-      console.log(user.roles);
-
-      console.log(user.roles._id === role._id);
-      console.log(role._id);
 
       if (String(user.roles._id) === String(role._id)){
         return user;
