07b11565bd2ef96b8b57347635b6bddc086ec400
Esen Harris <esharris@bu.edu>
2018-08-31 16:48:39 -0400

    cleaner code

--- server/web/views/components/spreadsheet.jsx
+++ server/web/views/components/spreadsheet.jsx
@@ -2,33 +2,6 @@
 const React = require('react')
 const http = require('http')
 
-const divStyle = {
-  height: '100%',
-  width: '100%'
-}
-
-const gridclassname = "ag-theme-balham"
-const columnDefs = [
-{headerName: "Make", field: "make"},
-{headerName: "Model", field: "model"},
-{headerName: "Price", field: "price"}
-];
-
-// specify the data
-const rowDefs = [
-{make: "Toyota", model: "Celica", price: 35000},
-{make: "Ford", model: "Mondeo", price: 32000},
-{make: "Porsche", model: "Boxter", price: 72000}
-];
-//Test Data for user Data
-const columnDefs2 = [
-  {headerName:'User Id',field:'_id'},
-  {headerName: 'Created At',field:'createdAt'},
-  {headerName:'Email',field:'email'},
-  {headerName:'In Study',field:'inStudy'},
-  {headerName:'Name',field:'name'},
-  {headerName:'Username',field:'username'}];
-
 function grid(idname,columnData, route) {
   const scriptHtml =`
   // setup the grid after the page has finished loading
